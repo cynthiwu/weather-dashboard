e2c7678a96bb58f03e327a40705d54f0d010126a
Cynthia Wu <cynthia21wu@gmail.com>
2020-10-07 22:46:39 -0700

    Added ajax to populate main dashboard based on user search

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() { 
 let formInputEl = $("#formInput");
 let searchButtonEl = $("#searchButton");
+let citynameEl = $("#cityName");
+let dateEl = ("(" + moment().format("L") + ")");   
+
+
 
 // Setting up the initial load for the dashboard.
 
@@ -39,6 +43,7 @@ function searchCity(event) {
 
     // let newCity = searchedCities[searchedCities.length - 1];
     addListItem(newCity);
+    weatherSearch(newCity);
 
     }
 };
@@ -54,12 +59,78 @@ function addListItem(newItem) {
 }
 
 function weatherSearch(city) {
-    let queryUrl = formInputEl.val().trim();
+
+    let queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=31b6d28ef47d7d3a64ddd0263f83b5c2";
+    
+
     $.ajax({
-        url
-    })
+        url: queryUrl,
+        method: "GET",
+        crossDomain: true,
+        dataType: 'jsonp',
+    }).then(function(response) {
+        console.log(response);
+        citynameEl.text(response.name + "  " + dateEl);
+
+        let icon = response.weather[0].icon;
+        let iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
+        let newIcon = $("<img>");
+        newIcon.attr({src: iconURL, alt: "Today's weather icon."})
+        citynameEl.append(newIcon);
+
+        $("#cityTemp").text(tempConversion(response.main.temp));
+        $("#cityHumid").text(response.main.humidity + "%");
+        $("#currentWind").text(response.wind.speed.toFixed(1) + " MPH");
+        
+        let lat = response.coord.lat;
+        let lon = response.coord.lon;
+
+         
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=31b6d28ef47d7d3a64ddd0263f83b5c2",
+            method: "GET",
+            crossDomain: true,
+        }).then(function(response) {
+            let uvIndex = response.value;
+            let currentUV = $("#currentUV");
+            
+            console.log(response);
+            currentUV.text(uvIndex);
+
+            if (uvIndex >= 0 && uvIndex <= 2) {
+                currentUV.addClass("low");
+            }
+            else if (uvIndex > 2 && uvIndex <= 5) {
+                currentUV.addClass("moderate");
+            }
+            else if (uvIndex > 5 && uvIndex <= 7) {
+                currentUV.addClass("high");
+            }
+            else if (uvIndex > 7 && uvIndex <= 10) {
+                currentUV.addClass("veryhigh");
+            }
+            else if (uvIndex > 10) {
+                currentUV.addClass("extreme");
+            }
+        });
+    
+    });
+
+//class="bg-danger p-2 rounded"
+
+    
+
+};
+
+// Formula to conver kelvin to fahrenheit. 
+
+function tempConversion (kelvin) {
+
+    let newTemp = ((kelvin - 273.15) * 9/5 + 32).toFixed(1) + "°F";
+    return newTemp;
 
 }
+
 
 
 
