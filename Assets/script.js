67aa2235590dd00c6c85f614a779a837cbd8fe7d
Cynthia Wu <cynthia21wu@gmail.com>
2020-10-11 18:06:50 -0700

    Reworked code to separate functiions. Adding catch for errors for api calls.

--- Assets/script.js
+++ Assets/script.js
@@ -13,52 +13,184 @@ const searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
 // Setting up the initial load for the dashboard to show results for last search city. Default search will be for Seattle. Otherwise dashboard will display last city searched and load saved searches.
 
 if (searchedCities.length === 0) {
-    weatherSearch("Seattle");
-}
-else {
-    weatherSearch(searchedCities[searchedCities.length -1]);
+    weatherSearch("SEATTLE");
+    console.log("hello");
 }
 
-// Need conditional here in the case the last search as invalid. 
-  
 if (searchedCities.length > 0) {
+    weatherSearch(searchedCities[searchedCities.length - 1]);
     $.each(searchedCities, function(i, value){
         addListItem(value);
     })
 };
 
-// Function for searching for a city and following actions after the search buttin is clicked.
+
+// Function to validate the search input, preventing empty string. 
 
 function searchCity() {
-    
     let userInput = formInputEl.val().toUpperCase().trim();
+    console.log(userInput);
     
     //Preventing no answer
-
     if (userInput === "") {
         alert("Please enter a valid city name.");
-        // $('#myModal').modal("show");
+        // $('#myModal').modal("show"); Unhide and update the zindex/tabindex. Display none to start. 
     }
-        //Look into modal for this as opposed to alert.
-
-    // Preventing duplicates
-    else if (searchedCities.indexOf(userInput) !== -1) {
-        searchedCities.splice(searchedCities.indexOf(userInput), 1);
-        searchedCities.push(userInput);
-
-        removeListItem(userInput);
-        addListItem(userInput);
+        else {
         weatherSearch(userInput);
+    }
+};
+
+// Function for querying for input city and populating appropriate sections.
+
+function weatherSearch(city) {
+
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=31b6d28ef47d7d3a64ddd0263f83b5c2",
+        method: "GET",
+        crossDomain: true,
+        dataType: 'jsonp',
+    }).then(function(response) {
+        console.log(response);
+
+        // Preventing duplicates
+        checkDuplicate(city);
+
+        // addListItem(city);
+        citynameEl.text(response.name + "  " + dateEl);
+
+        // Creating image element to hold today's weather icon.
+        let icon = response.weather[0].icon;
+        let iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
+        let newIcon = $("<img>");
+        newIcon.attr({src: iconURL, alt: "Today's weather icon."})
+        citynameEl.append(newIcon);
+
+        // Populating today's weather data into the main dashboard. 
+        $("#cityTemp").text(tempConversion(response.main.temp));
+        $("#cityHumid").text(response.main.humidity + "%");
+        $("#currentWind").text(response.wind.speed.toFixed(2) + " MPH");
+        
+        // Creating lat and lon variables to be used in the 5 day ajax call.
+        let lat = response.coord.lat;
+        let lon = response.coord.lon;
+
+        getUVindex(lat, lon);
+        getFiveDay(city);
+    
+    }).catch(function(error) {
+        console.log(error);
+        const searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
+        if (searchedCities.length > 0) {
+            weatherSearch(searchedCities[searchedCities.length - 1]);
+        }
+        else {
+            citynameEl.text("Invalid City");
+        }
+    })
+    
+}
+
+function getUVindex(lat, lon) {
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=31b6d28ef47d7d3a64ddd0263f83b5c2",
+        method: "GET",
+        crossDomain: true,
+    }).then(function(response) {
+        let uvIndex = response.value;
+        let currentUV = $("#currentUV");
+        
+        console.log(response);
+        currentUV.text(uvIndex);
+
+        // Color-coding for today's UV index. 
+
+        if (uvIndex >= 0 && uvIndex <= 2) {
+            currentUV.removeClass().addClass("p-2 low");
+        }
+        else if (uvIndex > 2 && uvIndex <= 5) {
+            currentUV.removeClass().addClass("p-2 moderate");
+        }
+        else if (uvIndex > 5 && uvIndex <= 7) {
+            currentUV.removeClass().addClass("p-2 high");
+        }
+        else if (uvIndex > 7 && uvIndex <= 10) {
+            currentUV.removeClass().addClass("p-2 veryhigh");
+        }
+        else if (uvIndex > 10) {
+            currentUV.removeClass().addClass("p-2 extreme");
+        }
+    }).catch(function(error) {
+        console.log(error);
+        let currentUV = $("#currentUV");
+        currentUV.text("N/A");
+    })
+}
+
+// Ajax call for the 5 day weather forecast. 
+
+function getFiveDay(city) {
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=31b6d28ef47d7d3a64ddd0263f83b5c2",
+        method: "GET",
+        crossDomain: true,
+        dataType: 'jsonp',
+    }).then(function(response) {
+        
+        console.log(response);
+
+        // Adding date to the top of each card using moment.js.
+        let cardTitle = $(".card-title");
+        cardTitle.each(function(index) {
+            $(this).text(moment().add(index + 1, "days").format("l"));
+        });
+
+        // Adding the given day weather icon to each card. 
+        let fiveIcon = $(".fiveIcon");
+        fiveIcon.each(function(index) {
+            let icon = response.list[index].weather[0].icon;
+            let iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
+            let newIcon = $("<img>");
+            newIcon.attr({src: iconURL, alt: "Five day forecast weather icon."})
+            $(this).html(newIcon);
+        });
+       
+        // Adding the given day temp to each card.
+        let cardTemp = $(".card-temp");
+        cardTemp.each(function(index) {
+            $(this).text(tempConversion(response.list[index].main.temp));
+        });
+
+        // Adding the given day humidity.
+        let cardHum = $(".card-hum");
+        cardHum.each(function(index) {
+            $(this).text(response.list[index].main.humidity + "%");
+        });
+
+    }).catch(function(error) {
+        console.log(error);
+        let cardTitle = $(".card-title");
+        cardTitle.text("Nothing found");
+    })
+}
+
+function checkDuplicate(city) {
+    if (searchedCities.indexOf(city) !== -1) {
+        searchedCities.splice(searchedCities.indexOf(city), 1);
+        searchedCities.push(city);
+
+        removeListItem(city);
+        addListItem(city);
         localStorage.setItem("cities", JSON.stringify(searchedCities));
     }
 
     else {  
-        searchedCities.push(userInput);
+        searchedCities.push(city);
         localStorage.setItem("cities", JSON.stringify(searchedCities));
-        addListItem(userInput);
-        weatherSearch(userInput);
+        addListItem(city);
     }
-};
+}
+
 
 // Function for removing a duplicate list item from the search bar.
 
@@ -91,106 +223,7 @@ function navSearch() {
     weatherSearch($(this).data("city"));
 }
 
-function weatherSearch(city) {
-
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=31b6d28ef47d7d3a64ddd0263f83b5c2",
-        method: "GET",
-        crossDomain: true,
-        dataType: 'jsonp',
-    }).then(function(response) {
-        console.log(response);
-        
-        citynameEl.text(response.name + "  " + dateEl);
-
-        let icon = response.weather[0].icon;
-        let iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
-        let newIcon = $("<img>");
-        newIcon.attr({src: iconURL, alt: "Today's weather icon."})
-        citynameEl.append(newIcon);
-
-        $("#cityTemp").text(tempConversion(response.main.temp));
-        $("#cityHumid").text(response.main.humidity + "%");
-        $("#currentWind").text(response.wind.speed.toFixed(2) + " MPH");
-        
-        let lat = response.coord.lat;
-        let lon = response.coord.lon;
-
-         
-        $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=31b6d28ef47d7d3a64ddd0263f83b5c2",
-            method: "GET",
-            crossDomain: true,
-        }).then(function(response) {
-            let uvIndex = response.value;
-            let currentUV = $("#currentUV");
-            
-            console.log(response);
-            currentUV.text(uvIndex);
-
-            if (uvIndex >= 0 && uvIndex <= 2) {
-                currentUV.removeClass().addClass("p-2 low");
-            }
-            else if (uvIndex > 2 && uvIndex <= 5) {
-                currentUV.removeClass().addClass("p-2 moderate");
-            }
-            else if (uvIndex > 5 && uvIndex <= 7) {
-                currentUV.removeClass().addClass("p-2 high");
-            }
-            else if (uvIndex > 7 && uvIndex <= 10) {
-                currentUV.removeClass().addClass("p-2 veryhigh");
-            }
-            else if (uvIndex > 10) {
-                currentUV.removeClass().addClass("p-2 extreme");
-            }
-        });
-    
-    });
-
-// Ajax call for the 5 day weather forecast. 
-
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=31b6d28ef47d7d3a64ddd0263f83b5c2",
-        method: "GET",
-        crossDomain: true,
-        dataType: 'jsonp',
-    }).then(function(response) {
-        
-        console.log(response);
-
-        let cardTitle = $(".card-title");
-
-        cardTitle.each(function(index) {
-            $(this).text(moment().add(index + 1, "days").format("l"));
-        });
-
-        let fiveIcon = $(".fiveIcon");
-
-
-        fiveIcon.each(function(index) {
-            let icon = response.list[index].weather[0].icon;
-            let iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
-            let newIcon = $("<img>");
-            newIcon.attr({src: iconURL, alt: "Five day forecast weather icon."})
-            $(this).html(newIcon);
-        });
-       
-        let cardTemp = $(".card-temp");
-
-        cardTemp.each(function(index) {
-            $(this).text(tempConversion(response.list[index].main.temp));
-        });
-
-        let cardHum = $(".card-hum");
-
-        cardHum.each(function(index) {
-            $(this).text(response.list[index].main.humidity + "%");
-        });
-
-    });
-}
-
-// Formula to conver kelvin to fahrenheit. 
+// Formula to convert kelvin to fahrenheit. 
 
     function tempConversion (kelvin) {
         let newTemp = ((kelvin - 273.15) * 9/5 + 32).toFixed(2) + "°F";
@@ -198,7 +231,7 @@ function weatherSearch(city) {
     };
 
 
-// Click handler for the search button.
+// Click handler for the search and side-nav buttons.
     
     searchButtonEl.on("click", searchCity);
     navButtonEl.on("click", navSearch);
